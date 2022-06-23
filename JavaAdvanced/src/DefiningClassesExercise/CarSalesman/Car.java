0e864d201b89c4cc10840d3b0818d2f891b6b8b9
Alexanski13 <alexanderivanov344@gmail.com>
2022-06-23 10:50:53 +0300

    init

---
+++ JavaAdvanced/src/DefiningClassesExercise/CarSalesman/Car.java
@@ -0,0 +1,38 @@
+package DefiningClassesExercise.CarSalesman;
+
+public class Car {
+    private String model;
+    private Engine engine;
+    private String weight;
+    private String color;
+
+    public Car(String model, Engine engine, String weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return this.model + ":" + "\n" + String.format("%s" +
+                "  Weight: %s%n" +
+                "  Color: %s", this.engine, this.weight, this.color);
+    }
+}
---
+++ JavaAdvanced/src/DefiningClassesExercise/CarSalesman/Engine.java
@@ -0,0 +1,39 @@
+package DefiningClassesExercise.CarSalesman;
+
+public class Engine {
+    private String engineModel;
+    private String power;
+    private String displacement;
+    private String efficiency;
+
+    public Engine(String model, String power, String displacement, String efficiency) {
+        this.engineModel = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public String getEngineModel() {
+        return engineModel;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public String toString() {
+        return "  " + this.engineModel + ":" + "\n"+String.format(
+                "    Power: %s%n" +
+                        "    Displacement: %s%n" +
+                        "    Efficiency: %s%n", this.power, this.displacement, this.efficiency);
+    }
+}
